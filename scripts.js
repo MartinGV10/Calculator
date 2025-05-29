42afc23510073dac9bf06a12d451bf145fdf6938
MartinGV10 <mganen10@gmail.com>
2025-05-28 22:54:25 -0400

    Added reset functionality

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let baseValue = 0
+
+const display = document.querySelector('.display')
+
+const ac = document.querySelector('.reset')
+ac.addEventListener('click', () => {
+    display.textContent = 0
+})
